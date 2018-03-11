b001d16f8bcd4c44e984643c509175f91d76be11
Raghu Betina <raghu@firstdraft.com>
2018-03-11 18:55:08 +0000

    Whitelist columns in Ds53Ct for ActiveAdmin

--- app/admin/ds53_cts.rb
+++ app/admin/ds53_cts.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Ds53Ct do
+
+ permit_params :jan, :feb, :mar, :apr, :may, :jun, :jul, :aug, :sep, :oct, :nov, :dec, :name, :user_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
