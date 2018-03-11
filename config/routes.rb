d96556856a440a21170d0e7e35006cfbf21f747a
Raghu Betina <raghu@firstdraft.com>
2018-03-11 18:54:40 +0000

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "ds53_cts#index"
   # Routes for the Comment resource:
   # CREATE
   get "/comments/new", :controller => "comments", :action => "new"
