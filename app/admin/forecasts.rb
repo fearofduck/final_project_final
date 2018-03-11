7d524f7915d4816b5c71b2e5e66884fdcf576d9a
Raghu Betina <raghu@firstdraft.com>
2018-03-11 18:55:11 +0000

    Whitelist columns in Forecast for ActiveAdmin

--- app/admin/forecasts.rb
+++ app/admin/forecasts.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Forecast do
+
+ permit_params :ds53_growth, :ds40_growth, :ds53_ct_id, :user_id, :itl_in_53, :dc_in_conv, :non_ttx_adds, :name, :cars_required, :cars_req_greater_0, :ds53_idle
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
