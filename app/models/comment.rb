681c92ec0145447690cb133bf7abc1436330d48d
Raghu Betina <raghu@firstdraft.com>
2018-03-11 18:54:48 +0000

    Direct association between forecasts and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,8 @@
 class Comment < ApplicationRecord
   # Direct associations
 
+  belongs_to :forecast
+
   belongs_to :user
 
   # Indirect associations
--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,6 +1,9 @@
 class Forecast < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :dependent => :destroy
+
   belongs_to :ds53_ct
 
   belongs_to :user
